a35908ccb4b1edaaec39cf59cc38026c240c4ea5
louiskatze1 <41235018-louiskatze1@users.noreply.replit.com>
2025-11-30 19:25:52 +0000

    Restored to 'f3a66c0e526d4ad8dec360e12d7d9244517e2b23'
    Replit-Restored-To: f3a66c0e526d4ad8dec360e12d7d9244517e2b23

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { I18nextProvider } from "react-i18next";
 import { queryClient } from "./lib/queryClient";
-import i18n from "./i18n";
 import App from "./App";
 import "./index.css";
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
-  <I18nextProvider i18n={i18n}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </I18nextProvider>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
